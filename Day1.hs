0cbbf00e1f10a210850b004e39b2f01772ad8def
Ben Jacobson <benjacobsondev@gmail.com>
2024-12-08 22:25:24 -0800

    Format repo

--- Day1.hs
+++ Day1.hs
@@ -7,13 +7,13 @@ parser :: String -> Input
 parser input = map (map read . words) (lines input)
 
 part1 :: Input -> Int
-part1 input = sum $ map (\(x:y:xs) -> abs (x-y)) (transpose (map sort (transpose input)))
+part1 input = sum $ map (\[x, y] -> abs (x - y)) (transpose (map sort (transpose input)))
 
 count :: [[Int]] -> Int
-count [xs, ys] = sum $ filter (/=0) (map (\x -> length (filter (==x) ys) * x) xs)
+count [xs, ys] = sum $ filter (/= 0) (map (\x -> length (filter (== x) ys) * x) xs)
 
 part2 :: Input -> Int
 part2 input = count (transpose input)
 
-main :: IO()
+main :: IO ()
 main = interact $ runner parser part1 part2
