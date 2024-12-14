e29656159741fa894b4d50eca4096a77213dbd2d
Ben Jacobson <benjacobsondev@gmail.com>
2024-12-14 15:57:30 -0800

    Solve day 6

---
+++ Day6.hs
@@ -0,0 +1,73 @@
+import AoC (runner)
+import Data.Set (Set, empty, insert, member)
+import Debug.Trace (traceShow)
+import Utils (Dir (..), allPos, isInGrid, modifyAt, moveDir, setAt, turnRight)
+
+data Loc = Loc
+  { pos :: (Int, Int),
+    dir :: Dir
+  }
+  deriving (Show, Ord, Eq)
+
+data Cell = FREE | BLOCK
+  deriving (Show, Eq)
+
+type Input = ([[Cell]], Loc)
+
+parseLoc :: [String] -> (Int, Int) -> Loc
+parseLoc [] _ = error "Failed to parse location"
+parseLoc ("" : ss) (i, j) = parseLoc ss (i + 1, 0)
+parseLoc ((c : s) : ss) (i, j) = case c of
+  '^' -> Loc {pos = (i, j), dir = UP}
+  _ -> parseLoc (s : ss) (i, j + 1)
+
+mapCell :: Char -> Cell
+mapCell '#' = BLOCK
+mapCell _ = FREE
+
+parser :: String -> Input
+parser s = (map (map mapCell) grid, parseLoc grid (0, 0))
+  where
+    grid = lines s
+
+getCell :: [[Cell]] -> (Int, Int) -> Cell
+getCell grid pos
+  | isInGrid grid pos = grid !! fst pos !! snd pos
+  | otherwise = FREE
+
+setCell :: [[Cell]] -> (Int, Int) -> Cell -> [[Cell]]
+setCell grid (i, j) v
+  | isInGrid grid (i, j) = modifyAt grid i (\r -> setAt r j v)
+  | otherwise = grid
+
+pathLength :: Input -> Set (Int, Int) -> Int
+pathLength (grid, Loc {pos = pos, dir = dir}) seen
+  | not $ isInGrid grid pos = length seen
+  | otherwise = case getCell grid newPos of
+      BLOCK -> pathLength (grid, Loc {pos = pos, dir = turnRight dir}) seen
+      FREE -> pathLength (grid, Loc {pos = newPos, dir = dir}) (insert newPos seen)
+  where
+    newPos = moveDir dir pos
+
+part1 :: Input -> Int
+part1 input = pathLength input (insert (pos $ snd input) empty) - 1
+
+hasPathCycle :: Input -> Set Loc -> Bool
+hasPathCycle (grid, Loc {pos = pos, dir = dir}) seen
+  | not $ isInGrid grid pos = False
+  | otherwise = case getCell grid newPos of
+      BLOCK -> hasPathCycle (grid, Loc {pos = pos, dir = turnRight dir}) seen
+      FREE -> member newLoc seen || hasPathCycle (grid, newLoc) (insert newLoc seen)
+  where
+    newPos = moveDir dir pos
+    newLoc = Loc {pos = newPos, dir = dir}
+
+part2 :: Input -> Int
+part2 (grid, loc) =
+  length $
+    filter id $
+      map (\blockPos -> hasPathCycle (setCell grid blockPos BLOCK, loc) (insert loc empty)) $
+        filter (\p -> getCell grid p == FREE && p /= pos loc) (allPos grid)
+
+main :: IO ()
+main = interact $ runner parser part1 part2
--- Utils.hs
+++ Utils.hs
@@ -1,13 +1,34 @@
 module Utils
-  ( isInRange,
-    isInGrid,
+  ( Dir (..),
+    allPos,
     freqMap,
-    fromTupleList
+    fromTupleList,
+    isInGrid,
+    isInRange,
+    moveDir,
+    turnRight,
+    setAt,
+    modifyAt,
   )
 where
 
 import Data.List
 import Data.Map (Map, empty, insertWith)
+
+data Dir = UP | DOWN | LEFT | RIGHT
+  deriving (Show, Ord, Eq)
+
+turnRight :: Dir -> Dir
+turnRight UP = RIGHT
+turnRight RIGHT = DOWN
+turnRight DOWN = LEFT
+turnRight LEFT = UP
+
+moveDir :: Dir -> (Int, Int) -> (Int, Int)
+moveDir UP (i, j) = (i - 1, j)
+moveDir RIGHT (i, j) = (i, j + 1)
+moveDir DOWN (i, j) = (i + 1, j)
+moveDir LEFT (i, j) = (i, j - 1)
 
 isInRange :: Int -> Int -> Int -> Bool
 isInRange l h v = l <= v && v <= h
@@ -16,8 +37,20 @@ isInGrid :: [[a]] -> (Int, Int) -> Bool
 isInGrid [] _ = False
 isInGrid (g : gs) (i, j) = isInRange 0 (length gs) i && isInRange 0 (length g - 1) j
 
-freqMap :: Ord a => [a] -> Map a Int
+allPos :: [[a]] -> [(Int, Int)]
+allpos [] = []
+allPos (r : rs) = [(i, j) | i <- [0 .. length rs], j <- [0 .. length r - 1]]
+
+freqMap :: (Ord a) => [a] -> Map a Int
 freqMap = foldr (\x m -> insertWith (+) x 1 m) empty
 
-fromTupleList :: Ord a => [(a, b)] -> Map a [b]
+fromTupleList :: (Ord a) => [(a, b)] -> Map a [b]
 fromTupleList = foldr (\(a, b) m -> insertWith (++) a [b] m) empty
+
+setAt :: [a] -> Int -> a -> [a]
+setAt l i v = modifyAt l i (const v)
+
+modifyAt :: [a] -> Int -> (a -> a) -> [a]
+modifyAt [] _ _ = []
+modifyAt (x : xs) 0 f = f x : xs
+modifyAt (x : xs) i f = x : modifyAt xs (i - 1) f
